d85ff741f547a84a703d2b7fc60dbfb5aee52468
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-19 11:25:59 +0300

    commit tree e507b4af747cf6a363bd07572e9f1868d8aac127

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //create a function isPalindrome
+  // pass a string parameter to the function
+  //should return true if the string is palindrome
+  //should return false if is not
+  //deals with lowercase strings that are all letters and ignores quotattions and full stops
+  //for this test, we are going to use regular expressions
 }
 
 /* 
   Add your pseudocode here
+initialize a function called isPlandrome
+pass a parameter called str
+lowercase the string and use the regular expressions to remove unwanted strings
 */
 
 /*
   Add written explanation of your solution here
+  this solution will initialize a function isPalindrome and pass a parameter str. we then include a variable to check reqular 
+  expressions and remove them from our string and that happens after we convert the string to LowerCase
+  then we create another variable which is going to give us the reversed value
 */
+function isPalindrome(str){
+  let re = /[\W_]/g; 
+  let strToLowercase= str.toLowerCase().replace(re, '')
+
+  let reverseString = strToLowercase.split('').reverse().join('')
+
+  return reverseString === strToLowercase;
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
